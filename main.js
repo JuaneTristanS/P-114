bebe9422b8f551f91258e3b649be6fc666cc3bb0
JuaneTristanS <94048970+JuaneTristanS@users.noreply.github.com>
2022-10-06 09:16:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(450, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function snapshot() {
+    save("tophat_image.png");
+}
